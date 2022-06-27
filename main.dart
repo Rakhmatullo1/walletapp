edde992f7a89238eaa0ca06606f7477a8877d238
Rakhmatullo1 <93793312+Rakhmatullo1@users.noreply.github.com>
2022-06-28 01:33:23 +0500

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 // import 'package:month_year_picker/month_year_picker.dart';
 import 'package:provider/provider.dart';
-import 'package:walletapp/providers/user.dart';
 
+import 'screens/splashscreen.dart';
+import 'providers/auth.dart';
+import 'screens/authScreen.dart';
 import 'screens/setting_screen.dart';
 import 'screens/notification_screen.dart';
 import 'screens/transfer_more.dart';
@@ -11,6 +13,9 @@ import 'providers/notification.dart';
 import 'providers/transactions.dart';
 import 'screens/home.dart';
 import 'screens/transfer_screen.dart';
+import 'providers/user.dart';
+import 'screens/new_card_screen.dart';
+import 'screens/beginner.dart';
 
 void main() {
   runApp(const WalletApp());
@@ -24,37 +29,89 @@ class WalletApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
+          create: (context) => Auth(),
+        ),
+        ChangeNotifierProxyProvider<Auth, Data>(
           create: (context) => Data(),
+          update: (context, auth, previousCard) => previousCard!
+            ..updateAuth(
+                auth.token, previousCard == null ? [] : previousCard.data),
         ),
-        ChangeNotifierProvider(
-          create: ((context) => TransactionData()),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => UserData(),
-        ),
-        ChangeNotifierProvider(create: (context) => NotificationData())
+        ChangeNotifierProxyProvider<Auth, TransactionData>(
+            create: ((context) => TransactionData()),
+            update: (context, auth, previousTrans) {
+              if (auth.token != null) {
+                return previousTrans!
+                  ..update(auth.token!,
+                      previousTrans == null ? [] : previousTrans.data);
+              }
+              return previousTrans!;
+            }),
+        ChangeNotifierProxyProvider<Auth, UserData>(
+            create: (context) => UserData(),
+            update: (context, auth, previousUser) {
+              if (auth.userID != null) {
+                return previousUser!
+                  ..updateAuth(auth.token, auth.userID,
+                      previousUser == null ? [] : previousUser.data);
+              }
+              return previousUser!;
+            }),
+        ChangeNotifierProxyProvider<Auth, NotificationData>(
+            create: (context) => NotificationData(),
+            update: (context, auth, previousNots) {
+              if (auth.token != null) {
+                return previousNots!
+                  ..update(auth.token!,
+                      previousNots == null ? [] : previousNots.data);
+              }
+              return previousNots!;
+            })
       ],
-      child: MaterialApp(
-        // localizationsDelegates:
-
-        // MonthYearPickerLocalizations.localizationsDelegates,
-
-        theme: ThemeData(
-            fontFamily: 'QuickSand',
-            primaryColor: const Color.fromRGBO(132, 56, 255, 1)),
-        home: const Home(),
-        routes: {
-          Transfer.routeName: (context) {
-            return Transfer();
-          },
-          TransferMore.routeName: (context) {
-            var args = ModalRoute.of(context)?.settings.arguments;
-            return TransferMore(args);
-          },
-          NotificationScreen.routeName: (context) => const NotificationScreen(),
-          SettingScreen.routeName: (context) => const SettingScreen(),
-        },
-      ),
+      child: Consumer<Auth>(
+          builder: ((context, value, child) => MaterialApp(
+                theme: ThemeData(
+                    fontFamily: 'QuickSand',
+                    primaryColor: const Color.fromRGBO(132, 56, 255, 1)),
+                home: value.isAuth
+                    ? (Consumer<UserData>(builder: (context, user, child) {
+                        user.fetchAndSetData();
+                        for (int i = 0; i < user.data.length; i++) {
+                          if (user.data[i].creatorId == value.userID) {
+                            return Home(user.data[i].id);
+                          }
+                        }
+                        return const BeginnerScreen();
+                      }))
+                    : FutureBuilder(
+                        future: value.tryAutoLogin(),
+                        builder: ((context, snapshot) =>
+                            snapshot.connectionState == ConnectionState.waiting
+                                ? const SplashScreen()
+                                : AuthScreen())),
+                routes: {
+                  Transfer.routeName: (context) {
+                    return Transfer();
+                  },
+                  TransferMore.routeName: (context) {
+                    var args = ModalRoute.of(context)?.settings.arguments;
+                    return TransferMore(args);
+                  },
+                  NotificationScreen.routeName: (context) =>
+                      const NotificationScreen(),
+                  SettingScreen.routeName: (context) => const SettingScreen(),
+                  Home.routeName: (context) {
+                    final id =
+                        ModalRoute.of(context)!.settings.arguments as String;
+                    return Home(id);
+                  },
+                  NewCardScreeen.routeName: (context) {
+                    final id =
+                        ModalRoute.of(context)!.settings.arguments as String;
+                    return NewCardScreeen(id);
+                  },
+                },
+              ))),
     );
   }
 }
